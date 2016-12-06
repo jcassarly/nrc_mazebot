3105a56abfa2ec5ddadc991a522efa4706a56cb5
Jared Cassarly <jaredcassarly@case.edu>
2016-12-06 17:10:01 -0500

    added some comments

--- global_variables.cpp
+++ global_variables.cpp
@@ -1,9 +1,12 @@
 #include "global_variables.h"
 
+// the current direction the robot is moving (values 1-4 - see .h file for declarations)
 int direction = RIGHT;
 
+// distances the sensors pick up in centimeters
 double sensor_values[8] = {0}; 
 
+// wheel speeds correspodning to letter of variable (see README for which letter is which)
 int wheel_a = 0;
 int wheel_b = 0;
 int wheel_c = 0;
