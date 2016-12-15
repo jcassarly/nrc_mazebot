5f7c4965ad16f57d7dc138af647cd97fca27c834
Jonathan <walrus365@sbcglobal.net>
2016-12-15 17:57:20 -0500

    Update Wheel-Speed.cpp

--- Wheel-Speed.cpp
+++ Wheel-Speed.cpp
@@ -1,3 +1,35 @@
 #include "global.h"
 
-void setSpeed(int direction, int speed, 
+void setSpeed(int direction, int speed, int adjustment) //adjustment is for getParallel, which would calculate how much adjustment is needed
+{
+  //assuming pins are 1,2,3,4.  these will be changed later.  AnalogWrite takes (pin number, speed (0 - 255))
+  if(direction == 1) //"forward"
+  {
+   analogWrite(1, speed + adjustment);
+   analogWrite(2, speed + adjustment);
+   analogWrite(3, -speed + adjustment);
+   analogWrite(4, -speed + adjustment);
+  }
+  else if(direction == 2) //"right"
+  {
+   analogWrite(1, =speed + adjustment);
+   analogWrite(2, speed + adjustment);
+   analogWrite(3, speed + adjustment);
+   analogWrite(4, -speed + adjustment);
+  }
+  else if(direction == 3) //left
+  {
+   analogWrite(1, speed + adjustment);
+   analogWrite(2, -speed + adjustment);
+   analogWrite(3, -speed + adjustment);
+   analogWrite(4, speed + adjustment); 
+  }
+  else if(direction == 4) //backwards
+  {
+   analogWrite(1, -speed + adjustment);
+   analogWrite(2, -speed + adjustment);
+   analogWrite(3, speed + adjustment);
+   analogWrite(4, speed + adjustment); 
+  }
+               
+}
