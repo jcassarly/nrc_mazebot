19f9830bd110e0b22b5047a468228414704f6e6d
Jonathan <walrus365@sbcglobal.net>
2017-01-19 17:25:59 -0500

    Update Wheel-Speed.cpp

--- Wheel-Speed.cpp
+++ Wheel-Speed.cpp
@@ -1,35 +1,40 @@
 #include "global.h"
 
+int wheel1 = 1;
+int wheel2 = 2;
+int wheel3 = 3;
+int wheel4 = 4;
+
 void setSpeed(int direction, int speed, int adjustment) //adjustment is for getParallel, which would calculate how much adjustment is needed
 {
   //assuming pins are 1,2,3,4.  these will be changed later.  AnalogWrite takes (pin number, speed (0 - 255))
   if(direction == 0) //"forward"
   {
-   analogWrite(1, speed + adjustment);
-   analogWrite(2, speed + adjustment);
-   analogWrite(3, -speed + adjustment);
-   analogWrite(4, -speed + adjustment);
+   analogWrite(wheel1, speed + adjustment);
+   analogWrite(wheel2, speed + adjustment);
+   analogWrite(wheel3, -speed + adjustment);
+   analogWrite(wheel4, -speed + adjustment);
   }
   else if(direction == 2) //"right"
   {
-   analogWrite(1, =speed + adjustment);
-   analogWrite(2, speed + adjustment);
-   analogWrite(3, speed + adjustment);
-   analogWrite(4, -speed + adjustment);
+   analogWrite(wheel1, =speed + adjustment);
+   analogWrite(wheel2, speed + adjustment);
+   analogWrite(wheel3, speed + adjustment);
+   analogWrite(wheel4, -speed + adjustment);
   }
   else if(direction == 4) //left
   {
-   analogWrite(1, speed + adjustment);
-   analogWrite(2, -speed + adjustment);
-   analogWrite(3, -speed + adjustment);
-   analogWrite(4, speed + adjustment); 
+   analogWrite(wheel1, speed + adjustment);
+   analogWrite(wheel2, -speed + adjustment);
+   analogWrite(wheel3, -speed + adjustment);
+   analogWrite(wheel4, speed + adjustment); 
   }
   else if(direction == 6) //backwards
   {
-   analogWrite(1, -speed + adjustment);
-   analogWrite(2, -speed + adjustment);
-   analogWrite(3, speed + adjustment);
-   analogWrite(4, speed + adjustment); 
+   analogWrite(wheel1, -speed + adjustment);
+   analogWrite(wheel2, -speed + adjustment);
+   analogWrite(wheel3, speed + adjustment);
+   analogWrite(wheel4, speed + adjustment); 
   }
                
 }
