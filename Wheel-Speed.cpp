9cb8d9ed3df2e6c4d14684e4038be7f04077a463
Jonathan <walrus365@sbcglobal.net>
2017-01-23 17:53:12 -0500

    Update Wheel-Speed.cpp

--- Wheel-Speed.cpp
+++ Wheel-Speed.cpp
@@ -5,10 +5,43 @@ int wheel2 = 2;
 int wheel3 = 3;
 int wheel4 = 4;
 
-void setSpeed(int direction, int speed, int adjustment) //adjustment is for getParallel, which would calculate how much adjustment is needed
+void setSpeed(int direction, int speed, int adjustment, int veer_direction) //adjustment is for getParallel, which would calculate how much adjustment is needed
 {
   //assuming pins are 1,2,3,4.  these will be changed later.  AnalogWrite takes (pin number, speed (0 - 255))
-  if(direction == 0) //"forward"
+  
+  if(veer_direction != -1)
+  {
+   if(veer_direction == 0)
+   {
+   analogWrite(wheel1, -speed);
+   analogWrite(wheel2, -speed);
+   analogWrite(wheel3, speed);
+   analogWrite(wheel4, speed);
+   }
+   else if(veer_direction == 2)
+   {
+   analogWrite(wheel1, speed);
+   analogWrite(wheel2, -speed);
+   analogWrite(wheel3, -speed);
+   analogWrite(wheel4, speed);
+   }
+   else if(veer_direction == 4)
+   {
+   analogWrite(wheel1, -speed);
+   analogWrite(wheel2, speed);
+   analogWrite(wheel3, speed);
+   analogWrite(wheel4, -speed);
+   }
+   else if(veer_direction == 6)
+   {
+   analogWrite(wheel1, speed);
+   analogWrite(wheel2, speed);
+   analogWrite(wheel3, -speed);
+   analogWrite(wheel4, -speed);
+   }   
+  }
+  
+  else if(direction == 0) //"forward"
   {
    analogWrite(wheel1, speed + adjustment);
    analogWrite(wheel2, speed + adjustment);
@@ -17,7 +50,7 @@ void setSpeed(int direction, int speed, int adjustment) //adjustment is for getP
   }
   else if(direction == 2) //"right"
   {
-   analogWrite(wheel1, =speed + adjustment);
+   analogWrite(wheel1, -speed + adjustment);
    analogWrite(wheel2, speed + adjustment);
    analogWrite(wheel3, speed + adjustment);
    analogWrite(wheel4, -speed + adjustment);
