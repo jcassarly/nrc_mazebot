4f6fb6163df833f5112e410a66b744b1087c9d5c
Jared Cassarly <jaredcassarly@case.edu>
2017-02-02 17:18:09 -0500

    fixed indentation

--- get_direction.cpp
+++ get_direction.cpp
@@ -2,30 +2,30 @@
 #include "get_direction.h"
 
 void get_direction(int prev_dir){
-  int back;
-  int current;
-  int max_val=0;
-  int max_dir;
+    int back;
+    int current;
+    int max_val=0;
+    int max_dir;
 
-  //sets back to the direction we just came from
-  if (prev_dir<4)
-    back=prev_dir+4;
-  else
-    back=prev_dir-4;
+    //sets back to the direction we just came from
+    if (prev_dir<4)
+      back=prev_dir+4;
+    else
+      back=prev_dir-4;
 
-  //updates sensor_values array  
-  get_distance();
-  
-  for (int i=0; i<8; i+=2){
-    if (i!=back && i!=(back+1)){  //doesn't check direction we just came from
-      current=(int)(sensor_values[i]+sensor_values[i+1])/2; //sets current to the average of the two sensors pointing the same direction
-      if (current > max_val){ //if the current distance is greater than the max distance, max_val becomes current and max_dir becomes the current direction being checked
-        max_val=current;
-        max_dir=i;
-      }
+    //updates sensor_values array  
+    get_distance();
+    
+    for (int i=0; i<8; i+=2){
+        if (i!=back && i!=(back+1)){  //doesn't check direction we just came from
+            current=(int)(sensor_values[i]+sensor_values[i+1])/2; //sets current to the average of the two sensors pointing the same direction
+            if (current > max_val){ //if the current distance is greater than the max distance, max_val becomes current and max_dir becomes the current direction being checked
+                max_val=current;
+                max_dir=i;
+            }
+        }
     }
-  }
 
-  direction=max_dir;  //sets global variable to be the direction farthest away from a wall (the direction we need to move next)
+    direction=max_dir;  //sets global variable to be the direction farthest away from a wall (the direction we need to move next)
 
 }
--- move_in_direction.cpp
+++ move_in_direction.cpp
@@ -9,66 +9,72 @@ int wheel4 = 4;
 void move_in_direction(int direction, int speed, int adjustment, int veer_direction) //adjustment is for getParallel, which would calculate how much adjustment is needed
 {
     //assuming pins are 1,2,3,4.  these will be changed later.  AnalogWrite takes (pin number, speed (0 - 255))
-  
+
     if(veer_direction != -1)
     {
-     if(veer_direction == 0)
-     {
-       analogWrite(wheel1, -speed);
-       analogWrite(wheel2, -speed);
-       analogWrite(wheel3, speed);
-       analogWrite(wheel4, speed);
-     }
-     else if(veer_direction == 2)
-     {
-       analogWrite(wheel1, speed);
-       analogWrite(wheel2, -speed);
-       analogWrite(wheel3, -speed);
-       analogWrite(wheel4, speed);
-     }
-     else if(veer_direction == 4)
-     {
-       analogWrite(wheel1, -speed);
-       analogWrite(wheel2, speed);
-       analogWrite(wheel3, speed);
-       analogWrite(wheel4, -speed);
-     }
-     else if(veer_direction == 6)
-     {
-       analogWrite(wheel1, speed);
-       analogWrite(wheel2, speed);
-       analogWrite(wheel3, -speed);
-       analogWrite(wheel4, -speed);
-     }   
+        if(veer_direction == 0)
+        {
+            analogWrite(wheel1, -speed);
+            analogWrite(wheel2, -speed);
+            analogWrite(wheel3, speed);
+            analogWrite(wheel4, speed);
+        }
+        
+        else if(veer_direction == 2)
+        {
+            analogWrite(wheel1, speed);
+            analogWrite(wheel2, -speed);
+            analogWrite(wheel3, -speed);
+            analogWrite(wheel4, speed);
+        }
+
+        else if(veer_direction == 4)
+        {
+            analogWrite(wheel1, -speed);
+            analogWrite(wheel2, speed);
+            analogWrite(wheel3, speed);
+            analogWrite(wheel4, -speed);
+        }
+
+        else if(veer_direction == 6)
+        {
+            analogWrite(wheel1, speed);
+            analogWrite(wheel2, speed);
+            analogWrite(wheel3, -speed);
+            analogWrite(wheel4, -speed);
+        }   
     }
-    
+
     else if(direction == 0) //"forward"
     {
-       analogWrite(wheel1, speed + adjustment);
-       analogWrite(wheel2, speed + adjustment);
-       analogWrite(wheel3, -speed + adjustment);
-       analogWrite(wheel4, -speed + adjustment);
+        analogWrite(wheel1, speed + adjustment);
+        analogWrite(wheel2, speed + adjustment);
+        analogWrite(wheel3, -speed + adjustment);
+        analogWrite(wheel4, -speed + adjustment);
     }
+
     else if(direction == 2) //"right"
     {
-       analogWrite(wheel1, -speed + adjustment);
-       analogWrite(wheel2, speed + adjustment);
-       analogWrite(wheel3, speed + adjustment);
-       analogWrite(wheel4, -speed + adjustment);
+        analogWrite(wheel1, -speed + adjustment);
+        analogWrite(wheel2, speed + adjustment);
+        analogWrite(wheel3, speed + adjustment);
+        analogWrite(wheel4, -speed + adjustment);
     }
+
     else if(direction == 4) //left
     {
-       analogWrite(wheel1, speed + adjustment);
-       analogWrite(wheel2, -speed + adjustment);
-       analogWrite(wheel3, -speed + adjustment);
-       analogWrite(wheel4, speed + adjustment); 
+        analogWrite(wheel1, speed + adjustment);
+        analogWrite(wheel2, -speed + adjustment);
+        analogWrite(wheel3, -speed + adjustment);
+        analogWrite(wheel4, speed + adjustment); 
     }
+
     else if(direction == 6) //backwards
     {
-       analogWrite(wheel1, -speed + adjustment);
-       analogWrite(wheel2, -speed + adjustment);
-       analogWrite(wheel3, speed + adjustment);
-       analogWrite(wheel4, speed + adjustment); 
+        analogWrite(wheel1, -speed + adjustment);
+        analogWrite(wheel2, -speed + adjustment);
+        analogWrite(wheel3, speed + adjustment);
+        analogWrite(wheel4, speed + adjustment); 
     }
                
 }
